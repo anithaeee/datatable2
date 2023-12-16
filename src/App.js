6d5717ef50ffcb88b12c16d443818b056ec100f4
anithaeee <anithaeee224@gmail.com>
2023-12-16 15:31:33 +0800

    dec 16th

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Accordion, AccordionTab} from 'primereact/accordion';
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Accordion acctiveIndex={0}>
+        <AccordionTab header="Header 1">
+          <p className='p'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text commonly used 
+          to demonstrate the visual form of a document or a typeface without relying 
+          on meaningful content. Lorem ipsum may be used as a placeholder before final
+           copy is available.
+          </p>
+      </AccordionTab>
+      <AccordionTab header="Header 2">
+          <p className='p'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text commonly used 
+          to demonstrate the visual form of a document or a typeface without relying 
+          on meaningful content. Lorem ipsum may be used as a placeholder before final
+           copy is available.
+          </p>
+      </AccordionTab>
+      <AccordionTab header="Header 3">
+          <p className='p'>
+          n publishing and graphic design, Lorem ipsum is a placeholder text commonly used 
+          to demonstrate the visual form of a document or a typeface without relying 
+          on meaningful content. Lorem ipsum may be used as a placeholder before final
+           copy is available.
+          </p>
+      </AccordionTab>
+    </Accordion>
+
     </div>
   );
 }
